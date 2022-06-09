11095965bb3b106b19ae06337e2722b9a302e29a
João Vitor Fernandes <joaovitorcarvalhofernandes@hotmail.com>
2022-06-09 19:30:20 -0300

    Loja de Games

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,123 @@
+package com.generation.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // Código identificador do produto. 
+	
+	@NotBlank (message = "O campo nome do produto não pode ficar em branco!")
+	@Size (max = 30, message = "O campo só pode ser preenchido com no máximo 30 caracteres!")	
+	private String nome; // Nome do jogo. 
+	
+	@NotNull (message = "O campo nome deve ser obrigatóriamente preenchido!")
+	@Size (min = 10, max = 150, message = "O campo descrição deve conter no mínimo 10 e no máximo 150 caracteres!")	
+	private String descricao; // Breve descrição sobre o jogo. 
+	
+	@NotBlank
+	private int quantidade; // Quantidade de produto no estoque.
+	
+	@NotBlank
+	private double preco; // preço do produto.
+	
+	@NotBlank
+	private int parcela; // Quantidade de parcalas. 
+	
+	
+	private String foto; // Foto do produto.
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getParcela() {
+		return parcela;
+	}
+
+	public void setParcela(int parcela) {
+		this.parcela = parcela;
+	}
+	
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
---
+++ src/main/java/com/generation/lojagames/model/Usuario.java
@@ -0,0 +1,105 @@
+package com.generation.lojagames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name = "tb_usuarios")
+public class Usuario {
+
+	@Id
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	@NotBlank (message = "O campo nome é obrigatório")
+	@Size(min = 5, max = 100, message = "O nome deve conter de 5 a 100 caracteres")
+	private String nome;
+
+	@Email
+	@NotBlank (message = "O campo usuário é obrigatório")
+	private String usuario;
+	
+	@NotBlank (message = "O campo senha é obrigatório")
+	@Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres")
+	private String senha;
+	
+	private String foto;
+	
+	@NotNull
+	private String tipoUsuario;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties ("usuario")
+	private List<Produto>produto;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+}
